804d889584dc7e141e6ebcb7a956ea6adb102123
Trevor <trevorc212@gmail.com>
2020-02-19 21:09:42 -0500

    update

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ def main():
     File = open('Khalid_GEDCOM.txt')
     New_file = open("Gedcome-output.txt","w")
     data,New_file = createoutput(File,New_file)
-    New_file = test(data,New_file)
+    test(data,New_file)
     New_file.close()
     File.close()
 
