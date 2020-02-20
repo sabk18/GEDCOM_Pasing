ee484a13dffc6f0135a6ce1b7f4513b66cc537ca
Trevor <trevorc212@gmail.com>
2020-02-19 21:11:36 -0500

    Story 4

--- src/sprint1/story4.py
+++ src/sprint1/story4.py
@@ -1,3 +1,38 @@
-def test(info,output):
-    print ('Testing Data')
-    return output
+import unittest
+from datetime import datetime,timedelta
+class marrDateValidator(unittest.TestCase):
+    marrDate= datetime.now()
+    divDate= datetime.now()
+
+    def test_isdate(self):
+        result = testDates('false','nope')
+        self.assertFalse(result)
+    def test_same(self):
+        result = testDates(datetime.now(),datetime.now())
+        self.assertFalse(result)
+    def test_less(self):
+        result = testDates(datetime.now(),datetime.now()- timedelta(days=1))
+        self.assertFalse(result)
+    def test_more(self):
+        result = testDates(datetime.now(),datetime.now()+ timedelta(days=1))
+        self.assertTrue(result)
+    def test_isNull(self):
+        result = testDates(None,datetime.now()+ timedelta(days=1))
+        self.assertFalse(result)        
+
+def testDates(marr,div):
+    if not isinstance(marr, datetime) or not isinstance(div, datetime):
+        return False
+    if marr<div:
+        return True
+    else:
+        return False
+
+def test(info,file):
+    for fam in info['FAM']:
+        if 'MARR' in info['FAM'][fam] and 'DIV' in info['FAM'][fam]:
+            testDates(info['FAM'][fam]['MARR'],   info['FAM'][fam]['DIV'])
+
+
+if __name__ == '__main__':
+    unittest.main()
