97496e3fbe1ab902e00204bc623fcc75800539dc
dagarh <hdagar3@gmail.com>
2020-03-22 23:13:47 +0530

    Formatted the code as per python convention, appended few records in Khalid_GEDCOM file to implement my s22 user story and added encoding format while opening the file and writing to a file, so that it can run on all python versions & we could also debug the code now

--- run.py
+++ run.py
@@ -3,14 +3,16 @@ from src.sprint1.story4 import s4test
 from src.sprint1.story1_peer import s1test
 from src.sprint1.story3 import s3test
 
+
 def main():
-    File = open('Khalid_GEDCOM.txt')
-    New_file = open("Gedcome-output.txt","w")
-    data,New_file = createoutput(File,New_file)
-    New_file = s4test(data,New_file)
-    New_file = s1test(data,New_file)
-    New_file.close()
-    File.close()
+    file = open('Khalid_GEDCOM.txt', 'r', encoding="utf-8")
+    new_file = open("Gedcome-output.txt", "w", encoding='utf-8')
+    data, new_file = createoutput(file, new_file)
+    new_file = s4test(data, new_file)
+    new_file = s1test(data, new_file)
+    new_file.close()
+    file.close()
+
 
 if __name__ == "__main__":
     main()
--- src/gedcom.py
+++ src/gedcom.py
@@ -6,18 +6,16 @@ from tabulate import tabulate
 import datetime
 from datetime import date
 
-
-
 # In[ ]:
 # Create a dict to store data
-info={
-    'FAM':{},
-    'INDI':{}
-
+info = {
+    'FAM': {},
+    'INDI': {}
 }
 
-# These are the fields that we need to track as per the first part of the assigment. Had to drop date to make collection function work.
-typelist=[
+# These are the fields that we need to track as per the first part of the assignment. Had to drop date to make
+# collection function work.
+typelist = [
     'INDI',
     'NAME',
     'SEX',
@@ -35,9 +33,10 @@ typelist=[
     'TRLR',
     'NOTE']
 
-currenttracker='';
+currenttracker = '';
 
-def createCollection(line,currenttracker):
+
+def create_collection(line, currenttracker):
     ''' Updates the dict and adds the values in. Structure is as follows
     info = {
         'FAM':{
@@ -64,7 +63,7 @@ def createCollection(line,currenttracker):
     global info
     if line[2] == 'FAM':
         info['FAM'][line[1]] = {}
-        currenttracker=line[1]
+        currenttracker = line[1]
         return currenttracker
     elif line[1] == 'DIV' or line[1] == 'MARR':
         info['FAM'][currenttracker][line[1]] = " ".join(line[2:])
@@ -72,130 +71,134 @@ def createCollection(line,currenttracker):
         info['FAM'][currenttracker][line[1]] = line[2]
     elif line[1] == 'CHIL':
         if 'CHIL' not in info['FAM'][currenttracker]:
-            info['FAM'][currenttracker][line[1]]=[]
+            info['FAM'][currenttracker][line[1]] = []
         info['FAM'][currenttracker][line[1]].append(line[2])
 
     elif line[2] == 'INDI':
         info['INDI'][line[1]] = {}
-        currenttracker=line[1]
+        currenttracker = line[1]
         return currenttracker
     elif line[1] in typelist and currenttracker in info['INDI']:
         if line[1] == 'FAMC':
-            if 'FAMC' not in  info['INDI'][currenttracker]:
-                info['INDI'][currenttracker]['FAMC']=[]
+            if 'FAMC' not in info['INDI'][currenttracker]:
+                info['INDI'][currenttracker]['FAMC'] = []
             info['INDI'][currenttracker]['FAMC'].append(" ".join(line[2:]))
         else:
             info['INDI'][currenttracker][line[1]] = " ".join(line[2:])
     return currenttracker
 
 
-
-
-
 # In[2]:
 
-def createoutput(File,New_file):
+def createoutput(file, new_file):
     global currenttracker
-
-
 
     # In[3]:
 
-
-    Lines = File.readlines()
-
+    lines = file.readlines()
 
     # In[4]:
 
-
-    L1_tags = ['NAME','SEX','BIRT','DEAT','FAMC','FAMS','MARR','HUSB','WIFE','CHILL','DIV']
-    L0_tags_1 =['INDI','FAM']
-    L0_tags_2 =['HEAD','TRLR','NOTE']
-    L2_tags =['DATE']
-    date_type =['BIRT','MARR','DEAT','DIV']
+    l1_tags = ['NAME', 'SEX', 'BIRT', 'DEAT', 'FAMC', 'FAMS', 'MARR', 'HUSB', 'WIFE', 'CHILL', 'DIV']
+    l0_tags_1 = ['INDI', 'FAM']
+    l0_tags_2 = ['HEAD', 'TRLR', 'NOTE']
+    l2_tags = ['DATE']
+    date_type = ['BIRT', 'MARR', 'DEAT', 'DIV']
 
     # In[5]:
-    x=0
+    x = 0
 
-    for line in Lines:
-    #print (line)
+    for line in lines:
+        # print (line)
 
         line = line.strip()
 
-        New_file.write("-->{}".format(line))
-        New_file.write('\n')
+        new_file.write("-->{}".format(line))
+        new_file.write('\n')
         line = line.split(" ")
-        if len(line)>2:
-            currenttracker=createCollection(line,currenttracker)
+        if len(line) > 2:
+            currenttracker = create_collection(line, currenttracker)
         elif line[1] in date_type:
-            Nline = Lines[x+1].strip().split(' ')
+            Nline = lines[x + 1].strip().split(' ')
             Nline[1] = line[1]
-            currenttracker=createCollection(Nline,currenttracker)
+            currenttracker = create_collection(Nline, currenttracker)
 
-
-
-        #print (line)
+        # print (line)
         level = line[0]
-        #print (level)
+        # print (level)
         tags = line[1]
-        #print (tags)
-        arguments =line[2:]
-        #print (arguments)
+        # print (tags)
+        arguments = line[2:]
+        # print (arguments)
         arg_ = " ".join(arguments)
 
         if level == '1':
-            if tags in L1_tags:
+            if tags in l1_tags:
                 new = 'Y'
-                New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-                New_file.write('\n')
+                new_file.write(
+                        "<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(
+                            arguments) + '\n')
+                new_file.write('\n')
             else:
-                new ='N'
-                New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-                New_file.write('\n')
+                new = 'N'
+                new_file.write(
+                        "<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(
+                            arguments) + '\n')
+                new_file.write('\n')
 
         if level == '2':
-            if tags in L2_tags:
+            if tags in l2_tags:
                 new = 'Y'
-                New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-                New_file.write('\n')
+                new_file.write(
+                        "<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(
+                            arguments) + '\n')
+                new_file.write('\n')
             else:
-                new ='N'
-                New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-                New_file.write('\n')
+                new = 'N'
+                new_file.write(
+                        "<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(
+                            arguments) + '\n')
+                new_file.write('\n')
         if level == '3':
             new = 'N'
-            New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-            New_file.write('\n')
-        if level =='0':
+            new_file.write(
+                    "<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(arguments) + '\n')
+            new_file.write('\n')
+        if level == '0':
 
-            if arg_ in L0_tags_1:
-                new ='Y'
-                New_file.write("<--"+ "".join(level) + "|" + "".join(arguments) + "|" +"".join(new)+ "|" + "".join(tags)+'\n')
-                New_file.write('\n')
+            if arg_ in l0_tags_1:
+                new = 'Y'
+                new_file.write(
+                        "<--" + "".join(level) + "|" + "".join(arguments) + "|" + "".join(new) + "|" + "".join(
+                            tags) + '\n')
+                new_file.write('\n')
             else:
-                if tags in L0_tags_2:
+                if tags in l0_tags_2:
                     new = 'Y'
-                    New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-                    New_file.write('\n')
+                    new_file.write("<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(
+                            arguments) + '\n')
+                    new_file.write('\n')
                 else:
                     new = 'N'
-                    New_file.write("<--"+ "".join(level) + "|" + "".join(tags) + "|" +"".join(new)+ "|" + "".join(arguments)+'\n')
-                    New_file.write('\n')
-        x+=1
+                    new_file.write("<--" + "".join(level) + "|" + "".join(tags) + "|" + "".join(new) + "|" + "".join(
+                            arguments) + '\n')
+                    new_file.write('\n')
+        x += 1
 
-    table=[]
+    table = []
     for person in info['INDI']:
-        row =[person,
-    info['INDI'][person]['NAME'],
-    info['INDI'][person]['SEX'],
-    info['INDI'][person]['BIRT'],]
+        row = [person,
+               info['INDI'][person]['NAME'],
+               info['INDI'][person]['SEX'],
+               info['INDI'][person]['BIRT'], ]
         date_time_str = info['INDI'][person]['BIRT']
         date_time_obj = datetime.datetime.strptime(date_time_str, '%d %b %Y')
-        row.append((date.today().year - date_time_obj.year - ((date.today().month, date.today().day) < (date_time_obj.month, date_time_obj.day)))+1)
+        row.append((date.today().year - date_time_obj.year - (
+                (date.today().month, date.today().day) < (date_time_obj.month, date_time_obj.day))) + 1)
         if 'DEAT' in info['INDI'][person]:
-            row.extend(['True',info['INDI'][person]['DEAT']])
+            row.extend(['True', info['INDI'][person]['DEAT']])
         else:
-            row.extend(['False','N/A'])
+            row.extend(['False', 'N/A'])
         if 'FAMC' in info['INDI'][person]:
             row.append(info['INDI'][person]['FAMC'])
         else:
@@ -206,14 +209,17 @@ def createoutput(File,New_file):
             row.append('N/A')
         table.append(row)
     print('Individaul')
-    New_file.write('\nIndividaul\n')
-    print(tabulate(table, headers=['ID', 'NAME','Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse',], tablefmt="grid"))
-    New_file.write(tabulate(table, headers=['ID', 'NAME','Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse',], tablefmt="grid"))
-    New_file.write('\nFamilies\n')
+    new_file.write('\nIndividaul\n')
+    print(tabulate(table, headers=['ID', 'NAME', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse', ],
+                   tablefmt="grid"))
+    new_file.write(
+            tabulate(table, headers=['ID', 'NAME', 'Gender', 'Birthday', 'Age', 'Alive', 'Death', 'Child', 'Spouse', ],
+                     tablefmt="grid"))
+    new_file.write('\nFamilies\n')
     print('Families')
-    table=[]
+    table = []
     for famId in info['FAM']:
-        row=[famId]
+        row = [famId]
         if 'MARR' in info['FAM'][famId]:
             row.append(info['FAM'][famId]['MARR'])
         else:
@@ -223,25 +229,25 @@ def createoutput(File,New_file):
         else:
             row.append('N/A')
         row.extend(
-            [   info['FAM'][famId]['HUSB'],
-                info['INDI'][info['FAM'][famId]['HUSB']]['NAME'],
-                info['FAM'][famId]['WIFE'],
-                info['INDI'][info['FAM'][famId]['WIFE']]['NAME']
-            ])
+                [info['FAM'][famId]['HUSB'],
+                 info['INDI'][info['FAM'][famId]['HUSB']]['NAME'],
+                 info['FAM'][famId]['WIFE'],
+                 info['INDI'][info['FAM'][famId]['WIFE']]['NAME']
+                 ])
         if 'CHIL' in info['FAM'][famId]:
             row.append(info['FAM'][famId]['CHIL'])
         else:
             row.append('N/A')
         table.append(row)
-    print(tabulate(table, headers=['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children'], tablefmt="grid"))
+    print(tabulate(table, headers=['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name',
+                                   'Children'], tablefmt="grid"))
 
-    New_file.write(tabulate(table, headers=['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name', 'Children'], tablefmt="grid"))
+    new_file.write(tabulate(table,
+                            headers=['ID', 'Married', 'Divorced', 'Husband ID', 'Husband Name', 'Wife ID', 'Wife Name',
+                                     'Children'], tablefmt="grid"))
 
-
-    return info,New_file
+    return info, new_file
 # In[ ]:
-
-
 
 
 # In[ ]:
